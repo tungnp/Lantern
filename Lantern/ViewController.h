92967ac3b97e11193b7e513ecbdca74a1e448631
tung nguyen <tungnp92@gmail.com>
2013-04-22 16:36:01 +0700

    Initial Commit

---
+++ Lantern/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lantern
+//
+//  Created by stevie nguyen on 4/22/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
